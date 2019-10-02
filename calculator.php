51a86dd4bd36c655d4a95e610b2e6a8ae6d90fff
Reinis <52842617+CaptainLV86@users.noreply.github.com>
2019-10-02 09:27:36 +0100

    Add files via upload

---
+++ calculator.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Calculator</title>
+<link rel="stylesheet" type="text/css" href="design.css">
+</head>
+<body>
+<div class="main">
+<form name="myForm">
+<input type="text" name="textview" class="textview" disabled>
+</form>
+<table>
+  <tr>
+    <td><input type="button" class="button" value="1"></td>
+    <td><input type="button" class="button" value="2"></td>
+    <td><input type="button" class="button" value="3"></td>
+    <td><input type="button" class="button" value="+"></td>
+  </tr>
+  <tr>
+    <td><input type="button" class="button" value="4"></td>
+    <td><input type="button" class="button" value="5"></td>
+    <td><input type="button" class="button" value="6"></td>
+    <td><input type="button" class="button" value="-"></td>
+  </tr>
+  <tr>
+    <td><input type="button" class="button" value="7"></td>
+    <td><input type="button" class="button" value="8"></td>
+    <td><input type="button" class="button" value="9"></td>
+    <td><input type="button" class="button" value="*"></td>
+  </tr>
+  <tr>
+    <td><input type="button" class="button" value="."></td>
+    <td><input type="button" class="button" value="0"></td>
+    <td><input type="button" class="button" value="C"></td>
+    <td><input type="button" class="button" value="/"></td>
+</tr>
+<tr>
+  <td><input type="button" class="button1" value="="></td>
+</tr>
+
+</table>
